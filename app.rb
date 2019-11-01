9b552a6fb17de68443c45f889efbd1afd4b5cdeb
TaigaMikami <mikamimikami60@gmail.com>
2019-11-01 17:41:36 +0900

    DBの設定の追加

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'yaml'
 require 'slim'
+
+db_config_file = File.join(File.dirname(__FILE__), "app", "database.yml")
+if File.exist?(db_config_file)
+  config = YAML.load(File.read(db_config_file))
+  DB = Sequel.connect(config)
+  Sequel.extension :migration
+end
+
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each{ |file| require file }
 Dir[File.join(File.dirname(__FILE__), 'app', '**','*.rb')].each{ |file| require file }
 ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), 'app', 'routes.yml')))
+
+if DB
+  Sequel::Migrator.run(DB, File.join(File.dirname(__FILE__), 'app', 'db', 'migrations'))
+end
 
 require './lib/router'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < Controller
   def index
-
+    @posts = Post.all
   end
 end
